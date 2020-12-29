987387b5f24a7b92ec2fc2432e7e8ef0feb27890
stylecogan28 <76465599+stylecogan28@users.noreply.github.com>
2020-12-29 11:54:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1408,18 +1408,6 @@ Silakan pilih :
 *Selesaikan perintah untuk melakukan TOD selanjutnya* ⚠️` ,MessageType.text, {quoted: m});
 }
 
-//tagall
-if (!isGroup) return reply(mess.only.group)
-if (!isGroupAdmins) return reply(mess.only.admin)
-members_id = []
-teks = (args.length > 1) ? body.slice(8).trim() : ''
-teks += '\n\n'
-for (let mem of groupMembers) {
-rchoice = Math.floor(Math.random() * list_emoji.length)
-teks += `┣❥ @${mem.jid.split('@')[0]}\n`
-members_id.push(mem.jid)
-}
-mentions(teks, members_id, true)
 
 //Hay gay
 //create @mrf.zvx don't delate this please
